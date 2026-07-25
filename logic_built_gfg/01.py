1b5090e8f61ea0ae07b08d024afd073c0fe63971
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-25 23:49:59 +0530

    Update 01.py

--- logic_built_gfg/01.py
+++ logic_built_gfg/01.py
@@ -1,2 +1,13 @@
+# Question1 
 #even or odd 
+# class Solution:
+#     def isEven (self, n):
 
+# solution
+class Solution:
+    def isEven (self, n):
+        # code here 
+        if(n%2 ==0):
+            return True
+        else :
+            return False
