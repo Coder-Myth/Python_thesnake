1334911b42822ee5331b2b53313033a4846f0079
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-25 23:42:08 +0530

    Update gfg160days_01.py

--- code_practice/gfg160days_01.py
+++ code_practice/gfg160days_01.py
@@ -1,15 +1,16 @@
-#day 2 add two numbers 
-
 class Solution:
     def getSecondLargest(self, arr):
         # code here
-        for i in range(len(arr)):
-            arr.sort()
-            
+        for i in range(len(arr)-1):
+            # arr.sort()
+            # large=arr[0]
             a= arr[i]
-            b= arr[i+1]
+            b=arr[i+1]
+            if a<b:
+                large= b
+                second_large=a
+            else:
+                return -1
             
-            if a>b:
-                return b
-                
-        return b
+        return large
+        # return second_large
