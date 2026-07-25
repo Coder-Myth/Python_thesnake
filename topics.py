e5155ac4bbaafbb1c58ade220349d42e6a159235
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-25 23:42:37 +0530

    Remove implementation of getSecondLargest method

--- topics.py
+++ topics.py
@@ -1,13 +1,1 @@
-class Solution:
-    def getSecondLargest(self, arr):
-        # code here
-        for i in range(len(arr)):
-            arr.sort()
-            
-            a= arr[i]
-            b= arr[i+1]
-            
-            if a>b:
-                return b
-                
-        return b
+#topics for the python code 
