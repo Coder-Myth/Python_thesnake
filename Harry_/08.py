75c122e2ea3ce10664f6ff0df5f49a5f83157e63
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-28 00:55:07 +0530

    day08

---
+++ Harry_/08.py
@@ -0,0 +1,10 @@
+# typecasting : it is the intentionally converting one dta type to another datatype 
+
+# explicit : forcefully converting one data tyoe into another 
+
+# implicit : automatically python interpreter converts one data type into another 
+
+a="6"#a = string 6
+
+b=int(a)#b a int 6 (explicit typecasting)
+
