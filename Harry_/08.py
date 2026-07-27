8bb2a8999d9b299c4f25fe8d1741a9a6ff0165c0
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-28 00:57:41 +0530

    Update 08.py

--- Harry_/08.py
+++ Harry_/08.py
@@ -1,4 +1,17 @@
 # typecasting : it is the intentionally converting one dta type to another datatype 
 
 # explicit : forcefully converting one data tyoe into another 
- 
+
+
+# implicit : automatically python interpreter converts one data type into another 
+
+a="6"#a = string 6
+
+b=int(a)#b a int 6 (explicit typecasting)
+
+c=40.9
+
+d=54
+
+print(c+d)#here c float d is integer i.e d is converted into float i.e. implicit typecasting 
+
