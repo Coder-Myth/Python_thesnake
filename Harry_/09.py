c95741ca6cea79f950b922c966890d3b5dd68a02
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-28 01:06:10 +0530

    Update 09.py

---
+++ Harry_/09.py
@@ -0,0 +1,13 @@
+# taking user input 
+
+a=input("Enter Your Name")# input : Enter Your Name ---> Dhananjay Gurjar ---> now a = "Dhananjay Gurjar"
+
+print(a)#Dhananjay Gurjar
+
+# input takes the default as string 
+
+# typecasting is used to convert the input 
+
+b= int(input("Hello , Your Age : "))
+
+# now the input i got is in the form of integer 
