104ff7e4a530f6d4a5296c1d50ca5d4af5ffbe3c
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-27 17:30:43 +0530

    09

--- Harry_/05.py
+++ Harry_/05.py
@@ -1,6 +1,3 @@
 #escape sequence charecters : how to add the symbols in between the codes 
 
-
-# print statements: we use print statements to print the output we can have multiple string in one output 
-
-# 
+ 
