afada3ba638c2b56dca996abb2fd6cf9a9fc565c
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-27 17:28:06 +0530

    Update 05.py

--- Harry_/05.py
+++ Harry_/05.py
@@ -3,4 +3,8 @@
 
 # print statements: we use print statements to print the output we can have multiple string in one output 
 
-# 
+# print statements: we use print statements to print the output we can have multiple string in one output 
+
+# end we can get a sepcific statement symbol or anythinn where eer we want to have on it whenever it ts required
+
+#sep : using specific symbols for the statements to make the seperate is one of the task 
