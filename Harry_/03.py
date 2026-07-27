25a6980db639c1ce42d46bd35f8b4a4bef27e1f9
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-27 17:15:50 +0530

    03

---
+++ Harry_/03.py
@@ -0,0 +1,10 @@
+#modules & Pips 
+
+# modules: already written codes by programmer that allows you can borrow the code 
+
+# pips: managers it takes data from internet to your python interpreter 
+
+# pip install pandas in terminal ---> panda(an external module) is installed
+
+# two types of modules in built and external modules
+
