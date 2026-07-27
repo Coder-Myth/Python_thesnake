00ac3c6da42edf5d32791a40764f073e4c78784e
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-27 17:05:30 +0530

    final

---
+++ gfg_code/day02.py
@@ -0,0 +1,24 @@
+#type Conversion Method:
+#helps you inchanging the type of function from one to another 
+
+
+
+
+#tuple in python : creating single tuple in is a little tricky i.e. we nee to add omma after the list item i.e.
+#tuple=(10,)
+
+tuple=(1,2,3,4,5,56,7,8)
+
+print(tuple[-4])
+
+print(tuple[2:7])#starting from 2 and end at not 7
+
+print(tuple.index(56))
+
+print(tuple.count(5))
+
+#list 
+
+#list 
+list=[1,2,3,]
+
