6c33dda8e92c39248ba971d4d00316a29200257f
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-30 20:14:25 +0530

    Update 14.py

---
+++ Harry_/14.py
@@ -0,0 +1,20 @@
+# if  
+
+if n%2 ==0 :
+  print("Even")
+
+# if else 
+if a%4 ==0 :
+  print("leap Year")
+
+else :
+  print("Not Leap Year")
+
+# elif
+
+if a>=2:
+  print("A is Greater Than 2")
+elif a == 2:
+   print("A is equal to 2 ")
+else:
+  print("A is Smaller Than 2")
