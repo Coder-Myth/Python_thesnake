9ef8ff856179b3682cb42e8dd5eec22cffa69986
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-31 21:20:15 +0530

    day20

---
+++ Harry_/20.py
@@ -0,0 +1,27 @@
+#function :body of code that can be used again and again without rewriting code 
+
+def is_greater (a , b ):
+    if(a>b):
+        print("A is great")
+
+    else:
+        print("B is great")
+
+# code can be reused by just calling function 
+
+is_greater(8 , 14)
+
+
+# if function is left incomplete then  use pass to write it later and just make the functin run the code 
+
+
+def hello():
+    a="bd"
+    pass
+
+
+# 170/22
+
+# two types of function:
+# inbuilt function : already there in python 
+# user defined function : as per need they are defined 
