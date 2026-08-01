06a4e42a9e8db0eb0d241d64b7985b80cb62ac62
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-01 17:55:37 +0530

    Update 22.py

--- Harry_/22.py
+++ Harry_/22.py
@@ -41,3 +41,7 @@ print(list[1:-1])#negative indexing
 # on the fly generating a list 
 
 # there was no list before but as i got need i  just started making list 
+
+# there was no list before but as i got need i  just started making list 
+
+ 
