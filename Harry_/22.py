3eb3b6ba3c87255b76ddc3b44a67ad351441d1e9
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-01 17:45:14 +0530

    Update 22.py

--- Harry_/22.py
+++ Harry_/22.py
@@ -8,4 +8,20 @@ print(list)
 # index : accessing different items from list /
 
 # negative indexing ---> positive indexing  len(name)-x
- 
+ # data type
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Harry", True]
+print(list)
+
+# list : changeable,  ordered , store multiple item in single variable
+
+# index : accessing different items from list /
+
+# negative indexing ---> positive indexing  len(name)-x
+
+# check something in python
+
+if 4 in list:
+    print("Yes")
+else:
+    print("No") 
