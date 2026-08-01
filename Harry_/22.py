d8fdfac382a5162a176e9ba48e31f0cf0d4cb875
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-01 17:44:22 +0530

    day22

---
+++ Harry_/22.py
@@ -0,0 +1,11 @@
+# data type
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Harry", True]
+print(list)
+
+# list : changeable,  ordered , store multiple item in single variable
+
+# index : accessing different items from list /
+
+# negative indexing ---> positive indexing  len(name)-x
+ 
