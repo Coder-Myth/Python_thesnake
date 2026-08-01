efee828b232edde2ff0b7384da24d354b00cf71a
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-01 16:59:43 +0530

    Update 21.py

--- Harry_/21.py
+++ Harry_/21.py
@@ -9,4 +9,13 @@
 
 def Code(a=9, c=14):
     print(a+c)
+
+Code(34 , 45)#a=9,b=14 ignored but 
+
+Code(34)#a=34, b=14
+
+Code(c=98)#a=9, b=98
+
+#keywords Arguments : order doesnot matter if the given argument is in the way given below
+Code(c=46, a=76)#keyword argument
  
