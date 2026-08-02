5ad3aafb3bef88650b577319973b9bb3a0afd8ad
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-03 03:15:16 +0530

    Update 23.py

--- Harry_/23.py
+++ Harry_/23.py
@@ -19,4 +19,11 @@ list.sort()  # sort the elements ascending order
 
 list.sort(reverse=True)  # ----
 #                          #   |---> sort list elements in descending order
-list.reverse()  # -
+list.reverse()  # - ------------
+
+list.index(5)  # index starts fom zero and moves to the 5 and give its output
+
+list.count("#value")  # count the times value inseted comes in it
+
+list.pop(7) #removes 7 from list
+ 
