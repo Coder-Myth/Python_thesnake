8148ab3abbdb1007e74b69fc2feb1ca6b0a9ac6f
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-05 01:19:14 +0530

    day27

--- Harry_/27.py
+++ Harry_/27.py
@@ -2,16 +2,33 @@
 
 questions= ["2+4", "If time is 12:00PM will it morning or afternoon", "4/2"]
 
+final_score =0
+
 for i in range(0, len(questions)):
     indexing=questions[i]
     print(indexing)
     ans1=input("Enter Your Answer:")
-    final_score=0
+    score=0
     if(ans1==6):
-        final_score+=1
+        score+=1
+        final_score=final_score+score
     else:
         final_score=0
      
     
     for j in range(): 
         pass
+
+def display_Que():
+    for i in range(0, len(questions)):
+        indexing=questions[i]
+        print(indexing)
+
+def check_answers():
+
+    if answer1==6:
+
+
+def calculate_score():
+    pass
+
