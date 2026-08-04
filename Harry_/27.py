cf786c2df83cda0c5c7d6650d877cbca71f91510
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-05 04:08:25 +0530

    day28

--- Harry_/27.py
+++ Harry_/27.py
@@ -1,4 +1,4 @@
-# kbc : create a program that asks you questions and tells you marks accordingly or the final score on this basis 
+# kbc : create a program that asks you questions and tells you marks accordingly or the final score on this basis
 
 questions = ["2+4", "16*2", "4/2"]
 
@@ -9,22 +9,20 @@ for i in range(0, len(questions)):
     print(indexing)
     answer.append(int(input("Enter Your Answer:")))
 
- 
 score = []
 
 for j in range(0, len(answer)):
     check_answer = answer[j]
     if j == 0 and check_answer == 6:
         score.append(1)
-            elif j == 1 and check_answer == 32:
+    elif j == 1 and check_answer == 32:
         score.append(1)
     elif j == 2 and check_answer == 2:
         score.append(1)
     else:
         score.append(0)
     # print(check_answer)
- 
- 
+
 # print(score)
 # print(answer)
 
@@ -33,6 +31,5 @@ final_score = 0
 for k in range(0, len(score)):
     score_for_each = score[k]
     final_score = final_score + score_for_each
- 
 
 print("Your Final Score is:", final_score)
---
+++ Harry_/28.py
@@ -0,0 +1,9 @@
+# f-strings: method to add variable in print stattements
+
+# in old days
+a = "Hello My Name {}, I'm from {}"
+
+name = "Dhananjay"
+country = "India"
+
+print(a.format(name, country))  # or use index in the place of writing variable names
