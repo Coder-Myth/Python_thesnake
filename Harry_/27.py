2c136e33ff101897438e3a1bded9d6bd7e6b95f5
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-05 03:49:55 +0530

    day27

--- Harry_/27.py
+++ Harry_/27.py
@@ -1,34 +1,6 @@
-#kbc : create a program that asks you questions and tells you marks accordingly
+# kbc : create a program that asks you questions and tells you marks accordingly
 
-questions= ["2+4", "If time is 12:00PM will it morning or afternoon", "4/2"]
+questions = ["2+4", "16*2", "4/2"]
 
-final_score =0
-
-for i in range(0, len(questions)):
-    indexing=questions[i]
-    print(indexing)
-    ans1=input("Enter Your Answer:")
-    score=0
-    if(ans1==6):
-        score+=1
-        final_score=final_score+score
-    else:
-        final_score=0
-     
-    
-    for j in range(): 
-        pass
-
-def display_Que():
-    for i in range(0, len(questions)):
-        indexing=questions[i]
-        print(indexing)
-
-def check_answers():
-
-    if answer1==6:
-
-
-def calculate_score():
-    pass
-
+answer = []
+ 
