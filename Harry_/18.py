6d1f809f29d4aca49f3eba06b39a9acceb20d906
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-04 22:00:14 +0530

    Update 18.py

--- Harry_/18.py
+++ Harry_/18.py
@@ -2,3 +2,7 @@ for i in range(1,4):
   if i==4:
     break# if  this is true then loop terminates
  
+
+  elif(i==3):
+    continue#skips 3 as value and continues the loop the same way as they way
+     
