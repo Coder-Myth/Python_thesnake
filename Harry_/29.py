d238b37c02fa74aef18f02b786b1e2ad180ec418
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-05 04:20:36 +0530

    day28

---
+++ Harry_/29.py
@@ -0,0 +1,16 @@
+# doc string: it shows the hidden line just beneath the given function 
+
+def sum(a,b):
+    '''Gives the Output as sum of a and b '''
+    print(a+b)
+
+sum(4,5)
+
+print(sum.__doc__)#this gives the line written in comment just below the function defined if any other line than comment comes that is not a doc string
+
+
+#PEP8: python enhancement proposal --->  way to make a python program more efficient ,readable
+
+# simply it is using import in your code 
+
+import this
