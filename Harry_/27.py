4788ed701cac5c63dc0c968375fbb61b4f099590
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-05 03:50:56 +0530

    Update 27.py

--- Harry_/27.py
+++ Harry_/27.py
@@ -3,4 +3,10 @@
 questions = ["2+4", "16*2", "4/2"]
 
 answer = []
+
+for i in range(0, len(questions)):
+    indexing = questions[i]
+    print(indexing)
+    answer.append(int(input("Enter Your Answer:")))
+
  
