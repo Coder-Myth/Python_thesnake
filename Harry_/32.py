3bf8057aa1db3de76058dae10e73fad9305633d0
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-06 01:26:32 +0530

    Implement set intersection and update operations
    Added intersection and intersection_update operations for sets.

--- Harry_/32.py
+++ Harry_/32.py
@@ -8,4 +8,15 @@ s2={4,5,6}
 union =s1.union(s2)
 
 update = s1.update(s2)# now s1 set contains all the value there in both set
+
+
+print(union)
+
+s3={1,2,3,4,57}
+intersection =s3.intersection(s2)
+
+print(intersection)
+
+intersection_update= s2.intersection_update(s1)
+# print(intersection_update)-----> output none because same elements that is the proof btw if you think it in a way
  
