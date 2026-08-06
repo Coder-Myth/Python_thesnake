76e5bec56c13bb4d8c51ab32f9bb8be80544a579
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-07 04:08:37 +0530

    Update 38.py

--- Harry_/38.py
+++ Harry_/38.py
@@ -10,4 +10,9 @@ if b == "quit":
     print("Yeahhh")
 elif not b.isdigit():
     raise ValueError("Not An Integer")
- 
+ else:
+    b=int(b)
+    if (b > 5) and (b < 9):
+        print(b)
+    else:
+        raise ValueError
