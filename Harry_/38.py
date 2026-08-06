e8484ad0b8d360267d4fa104dbc14272db7a02ec
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-07 04:08:28 +0530

    Update 38.py

--- Harry_/38.py
+++ Harry_/38.py
@@ -1,4 +1,13 @@
 # raise error : whenever required error to be there as a custom
 
 a = int(input("Enter Your Number that should be less that 10:"))
+
+if a >= 10:
+    raise ValueError("Invalid input \n Enter Your Number Below 10")
+
+b = input("Number b/w 5&9:")
+if b == "quit":
+    print("Yeahhh")
+elif not b.isdigit():
+    raise ValueError("Not An Integer")
  
