b2f389aa0bf5633789f12640ec7f03f96834698a
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-07 04:06:14 +0530

    Update 36.py

--- Harry_/36.py
+++ Harry_/36.py
@@ -6,4 +6,14 @@ a= int(input("Enter Your Number:"))
 for i in range(1,11):
     print(f"{a} X {i} = {a*i}")
 
+  # if input in a is not an integer then this shows error 
+
+
+try:
+    n = int(input("Number:"))
+    for i in range(1,11):
+        print(f"{n} X {i} = {n*i}")
+
+except ValueError:
+    print("Invalid Input")
  
