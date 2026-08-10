8bd23380b09cca78a417b84ade8567f26d6732f3
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-10 23:45:58 +0530

    Update day01.py

--- gfg_code/day01.py
+++ gfg_code/day01.py
@@ -9,3 +9,4 @@ a=int(input("Enter Your Image: "))
 # sep used to seperate, end used to end statements , multiple statements in one print statements
 #input Function i.e. a=input("....") stores value as string by default 
 #dt the end 
+#you will practice 
