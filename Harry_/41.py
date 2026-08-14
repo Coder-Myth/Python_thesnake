4a7c4779986d5b149f213b74b330af4b229ac2e2
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-14 21:37:15 +0530

    day29

---
+++ Harry_/41.py
@@ -0,0 +1,19 @@
+#short hand if else:
+#must use this for only short singlt hand statements  not for more than one statement 
+
+a= 45
+b=884
+
+print("A") if a>b else print("Equal") if a==b else print("B")
+# <---1--><-2---><------------3-------------><------4------>
+
+# 1==> statement to be excecuted 2==> if + condition behind it 3==>else + statement + if + condition
+
+# also can be used a sa conditionals 
+
+
+# use this as conditional statements
+c="fuck" if a<b else ""# double quote returns nothing 
+# if == true ----> c is printed and if not else statement is printed 
+
+print(c)
---
+++ Harry_/42.py
@@ -0,0 +1,6 @@
+#enumerate function :
+# In built python function that allows you to access the index as well the value at the same time 
+
+lst= [1,2,3,4,5,6,7,8,9]
+
+index =1
