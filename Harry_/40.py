64896f2d135fc10b364bae027fddd80c244e4be7
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-14 05:45:37 +0530

    day29

--- Harry_/40.py
+++ Harry_/40.py
@@ -1,7 +1,11 @@
-#excercise 2 ( code with harry )
+# excercise 2 ( code with harry )
 # excercise 4solution
 
-#code decode : instructions given 
+# code decode : instructions given
+# length word = less than 3 than print word
+# more than 3 word append first alphabet at the last
+
+
 task = input("1. Code \n\n2. Decode\n\nENTER YOUR CHOICE>>>>>>>>>>>")
 
 if not task.isdigit():
@@ -9,12 +13,22 @@ if not task.isdigit():
 
 elif task.isdigit():
     task = int(task)
+
     try:
         if task == 1:
-            pass
-        elif(task ==2):
+            word = input("Enter Your Message:")
+            if len(word) < 3:
+                print(word)
+            else:
+                for i in range(0, len(word)+1):
+                    # store this word into list each word in list then append then first word at last and then gain we will print output
+                    print(word[i],end="")
+
+
+        elif task == 2:
             print(
                 "1. word less than 3 words reversed\n\n 2. atleast or more than three words \n (a)fisrt word at the last and three random modules present at the end ans well as front "
             )
+
     except:
         print("Enter 1 or 2 as your integer:")
---
+++ Harry_/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+if task > 2:
+    #     print("Enter Your Number Between 1 and 2 \n \n retry")
+    # else:
