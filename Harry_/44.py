6885c1307d02496abdd7313242c804fd519f6bf0
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-15 22:34:17 +0530

    Update 44.py

--- Harry_/44.py
+++ Harry_/44.py
@@ -1,4 +1,9 @@
 # import 
 
 # import takes all the built in function in python 
-# 
+# import (name of library)
+
+# another way to import only specifice library 
+# from math import pi , sqrt 
+# this installs only pi and sqrt function from math
+
