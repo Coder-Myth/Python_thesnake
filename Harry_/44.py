66f9db7943524f3193e68491b5132454f2c88c7f
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-15 22:35:32 +0530

    Update 44.py

--- Harry_/44.py
+++ Harry_/44.py
@@ -7,3 +7,5 @@
 # from math import pi , sqrt 
 # this installs only pi and sqrt function from math
 
+# you can name the library when required to make it the way you want 
+# import numpy as np ----> numpy is named as np 
