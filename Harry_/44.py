3933b5f4ed5a6ab8e43d8a61cb2d8e5886b9ff71
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-15 22:36:24 +0530

    Update 44.py

--- Harry_/44.py
+++ Harry_/44.py
@@ -2,10 +2,13 @@
 
 # import takes all the built in function in python 
 # import (name of library)
+import math 
 
 # another way to import only specifice library 
 # from math import pi , sqrt 
 # this installs only pi and sqrt function from math
+from math import pi , sqrt 
 
 # you can name the library when required to make it the way you want 
 # import numpy as np ----> numpy is named as np 
+# now everywhere you can use it as np not numpy
