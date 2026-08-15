90e12dbfc0459bb55ac738b9fb9b504cda65a248
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-15 22:30:47 +0530

    day30

--- Harry_/43.py
+++ Harry_/43.py
@@ -1,1 +1,12 @@
 #virtual environment in python:
+# enviornmetn which is used when working on different projects should become easy 
+
+# this gives you a access to install the version that is different from globaal version 
+
+# to do this make a folder 
+# 1. open the folder in terminal :python3 -m venv (name of Environment)
+#   this takes you inside the folder 
+
+# 2. (name of Environment)/bin/activate this simply activates the environment
+
+# 3. deactivate ==> this one to deactivate
