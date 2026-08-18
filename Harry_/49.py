b4da7c5433a5b9f33b8e4c948f1ea9b7123b3136
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-18 11:55:11 +0530

    Update 49.py

--- Harry_/49.py
+++ Harry_/49.py
@@ -1,3 +1,3 @@
 # file I/O :handling a file 
 
-# open a file  (1. txt method , 2.Binary Method) , read , write , append ,close the file  ==>  
+# open a file  (1. txt method , 2.Binary Method) , read , write , append ,close the file  ==>  some manipulative commands 
