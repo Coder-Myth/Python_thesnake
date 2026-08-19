43e6a9b1963094bd31e1a2da25b716817f03cbc8
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-20 02:22:18 +0530

    Update 49.py

--- Harry_/49.py
+++ Harry_/49.py
@@ -26,9 +26,12 @@ d.write("Hello World!")
 
 
 
-# create : this creates the file if already exists then throws error
-
-
-
 # t and b ==> text and binary i.e. open file as txt i.e by default and binary form i.e. pdfs, images etc
 b = open("Locationoffile.txt", "rb")  # here r ---> read and b---> binary mode
+
+
+# shortcut method: 
+with open('sample.txt', 'w') as a:#any method can be used in this
+    a.write("hello World!2")
+
+    
