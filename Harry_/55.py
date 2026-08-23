2fe14866e6f396f99ed2ed0d0463566be30e9823
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-23 18:24:15 +0530

    Update 55.py

--- Harry_/55.py
+++ Harry_/55.py
@@ -2,6 +2,19 @@
 import random
 
 try:
+    while True:
+        user_inp = input(
+            "Enter \n1 for Rock \t\n2 for scissor\t\n3 for paper\t\n4 for Exiting\t\nEnter Your Choice>>>>>>>>>\n"
+        )
+        if user_inp.isdigit():
+            b = int(user_inp)
+            if b == 1 or b == 2 or b == 3:
+                    r = random.randint(1, 3) 
+            elif b == 4:
+                print("<<<<<<<<<<<<<See You Next Time>>>>>>>>>>>>>")
+                break
+            elif b > 4:
+                print("Enter Integer as\t\n\n 1 , 2 , 3\t")
 
 except:
     print("<----Restart the Game--->\t")
