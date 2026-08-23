fddb3b424abfab83a75a884754156de59caba6e6
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-23 18:25:59 +0530

    Update 55.py

--- Harry_/55.py
+++ Harry_/55.py
@@ -43,6 +43,11 @@ except:
 
 
  
+# input      1  2   3
+# rock    1  D  W   L
+# scissor 2  L  D   W
+# paper   3  W  L   D
+
 
 # the cleaner way for this is to make logic instead of the using numbers just like b==r i.e. draw then the three case in which i win keep them in or and use else to get other outputs
 
