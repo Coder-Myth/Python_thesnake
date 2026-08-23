f2e8bd9e3c918eb0f818fcdf6b91a27e0f98dd3d
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-23 18:22:21 +0530

    day35

--- Harry_/55.py
+++ Harry_/55.py
@@ -1,18 +1,19 @@
 # rock paper scissor:
+import random
 
-try:
-    while True:
-        user_inp = input(
-            "Enter \n\n1 for Rock \t\n\n2 for scissor\t\n\n3 for paper\t\n\n4 for Exiting\t\n\nEnter Your Choice>>>>>>>>>\n"
-        )
-        if user_inp.isdigit():
-            b = int(user_inp)
-            if b == 1 or b == 2 or b == 3:
-                print("COde till here work")
-            elif b == 4:
-                print("<<<<<<<<<<<<<See You Next Time>>>>>>>>>>>>>")
-                break
-            elif b > 4:
-                print("Enter Integer as\t\n\n 1 , 2 , 3\t")
+
+
 except:
     print("<----Restart the Game--->\t")
+
+
+
+# input      1  2   3
+# rock    1  D  W   L
+# scissor 2  L  D   W
+# paper   3  W  L   D
+
+
+# the cleaner way for this is to make logic instead of the using numbers just like b==r i.e. draw then the three case in which i win keep them in or and use else to get other outputs
+
+#  give your output to ai and then see what are the mistake if it could be the professional generate the code by youself first
