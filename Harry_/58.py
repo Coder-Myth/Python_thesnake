a0fd0dc8ca8ed40dec5ce13b93fb8d0233983b96
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-25 21:31:08 +0530

    day34

---
+++ Harry_/58.py
@@ -0,0 +1,4 @@
+#  constructor:  this is always called when there is an object created 
+#               this in terms helps to directly store value without giving varaible in different steps 
+
+#self is passed as an arguments
