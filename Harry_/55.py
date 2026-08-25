d069de94db9faea32a0af04b539ffab50beca54d
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-25 14:15:16 +0530

    day35

--- Harry_/55.py
+++ Harry_/55.py
@@ -9,29 +9,28 @@ try:
         if user_inp.isdigit():
             b = int(user_inp)
             if b == 1 or b == 2 or b == 3:
-                    r = random.randint(1, 3)
-                    # for rock as input
-                    if r == 1 and b == 1:
-                        print("Draw\n\n")
-                    elif r == 1 and b == 2:
-                        print("Win\n\n")
-                    elif r == 1 and b == 3:
-                        print("Lose\n\n") 
-                    # for scissor as input
-                    if r == 2 and b == 1:
-                        print("Lose\n\n")
-                    elif r == 2 and b == 2:
-                        print("Win\n\n")
-                    elif r == 2 and b == 3:
-                        print("Draw\n\n") 
-                    # for paper as input
-                    if r == 3 and b == 1:
-                        print("Win\n\n")
-                    elif r == 3 and b == 2:
-                        print("Lose\n\n")
-                    elif r == 3 and b == 3:
-                        print("Draw\n\n")
-                 
+                r = random.randint(1, 3)
+                # for rock as input
+                if r == 1 and b == 1:
+                    print("Draw\n\n")
+                elif r == 1 and b == 2:
+                    print("Win\n\n")
+                elif r == 1 and b == 3:
+                    print("Lose\n\n")
+                # for scissor as input
+                if r == 2 and b == 1:
+                    print("Lose\n\n")
+                elif r == 2 and b == 2:
+                    print("Win\n\n")
+                elif r == 2 and b == 3:
+                    print("Draw\n\n")
+                # for paper as input
+                if r == 3 and b == 1:
+                    print("Win\n\n")
+                elif r == 3 and b == 2:
+                    print("Lose\n\n")
+                elif r == 3 and b == 3:
+                    print("Draw\n\n")
             elif b == 4:
                 print("<<<<<<<<<<<<<See You Next Time>>>>>>>>>>>>>")
                 break
@@ -42,7 +41,6 @@ except:
     print("<----Restart the Game--->\t")
 
 
- 
 # input      1  2   3
 # rock    1  D  W   L
 # scissor 2  L  D   W
