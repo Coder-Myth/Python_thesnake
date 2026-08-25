d43819321cad2c265e48c77998062f793c9d8c78
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-25 18:36:43 +0530

    Update 57.py

--- Harry_/57.py
+++ Harry_/57.py
@@ -11,3 +11,9 @@ class info:  # class Blueprint
             f"{self.name} is in {self.Class} Class\nGoing to pursue {self.Branch} branch\n"
         )
 
+
+a = info()  # object in python one or as per need i.e. object1
+a.name = "Raja"
+a.Class = "12th"
+a.Branch = "cse"
+a.Details() 
