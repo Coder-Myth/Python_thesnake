db87c12492640f67f719bb0b4fda8201410b09ba
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-27 00:08:39 +0530

    day6

---
+++ practice/01.py
@@ -0,0 +1,31 @@
+# 01
+print("Hello World!\n")
+
+# 02
+name = input("Enter Your Name\n:")
+print("Hello",name,"\n")
+
+# 03
+a=int(input("Enter Your Number 1:"))
+b=int(input("Enter Your Number 2:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print("\n")
+
+# 04
+c=int(input("Enter Your Number 1:"))
+d=int(input("Enter Your Number 2:"))
+print(c)
+print(d)
+c,d=d,c
+print(c)
+print(d)
+
+# 05
+age=int(input("Enter Your Age :"))
+year_of_birth= 2026-age
+print("Your Year of Birth is :", year_of_birth,"\n")
+
+# 06
