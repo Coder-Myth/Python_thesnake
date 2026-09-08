925d3947187ad5197c7b5aa7d7f6f0d51e3fb68b
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-08 22:26:00 +0530

    day56

--- Harry_/tempCodeRunnerFile.py
+++ Harry_/tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-# # class and objects
+# class and objects
 
 
 # class info:  # class Blueprint
@@ -25,11 +25,3 @@
 # b.Branch = "Aiml" 
 # b.Details()
 
-
-class book():
-    book_name="Enter Your Book Name:\n"
-    book_author="Enter Your Book author:\n"
-    book_price="Enter Your Book price:\n"
-
-    def details_book(self):
-        print(f"The \"{self.book_name}\" is written by {self.book_author},\nthe best book under {self.book_price}")
