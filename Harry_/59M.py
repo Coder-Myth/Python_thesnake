3c16c1db78470b6f07a1ae1ea58b774d6f9879e2
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-09 03:47:13 +0530

    day56

--- Harry_/59M.py
+++ Harry_/59M.py
@@ -1,28 +1,28 @@
 # Decorators:
 # this function basically takes the function under @decorator as input and perform the other tasks
-
+# decorator basically helps function to be decorated in the class itself.
 
 def price_alltoment(fx):
-    def price_loading():
+    def price_loading(*args, **kwargs):# *arguments, * keywordsarguments
         print("price loading........\n")
-        fx()
+        fx(*args, **kwargs)
         print("Price Displayed")
 
     return price_loading
 
-
-@price_alltoment
 class book:
     def __init__(self, book_name, book_price, book_author):
         self.book_name = book_name
         self.book_price = book_price
         self.book_author = book_author
 
+    # @price_alltoment
     def info_books(self):
         print(
-            f" the book {self.book_name} written by {self.book_author} is best under {self.book_price}"
+            f"The book {self.book_name} written by {self.book_author} is best under {self.book_price}\n"
         )
 
 
+price_allotment(info_sbook)()
 book12 = book("venice ", 550, "shakespeare")
 book12.info_books()
