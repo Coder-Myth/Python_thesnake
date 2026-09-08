37a117422554c04e5544c09fb70f28e47b62b57c
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-09 03:19:19 +0530

    day56

--- Harry_/59M.py
+++ Harry_/59M.py
@@ -1,5 +1,28 @@
 # Decorators:
-# this is a function that when called it add the function 
+# this function basically takes the function under @decorator as input and perform the other tasks
 
-def hello():
- pass
+
+def price_alltoment(fx):
+    def price_loading():
+        print("price loading........\n")
+        fx()
+        print("Price Displayed")
+
+    return price_loading
+
+
+@price_alltoment
+class book:
+    def __init__(self, book_name, book_price, book_author):
+        self.book_name = book_name
+        self.book_price = book_price
+        self.book_author = book_author
+
+    def info_books(self):
+        print(
+            f" the book {self.book_name} written by {self.book_author} is best under {self.book_price}"
+        )
+
+
+book12 = book("venice ", 550, "shakespeare")
+book12.info_books()
--- Harry_/tempCodeRunnerFile.py
+++ Harry_/tempCodeRunnerFile.py
@@ -1,35 +1,10 @@
 
-# # self is passed as an arguments
-# class person:
-#     def __init__(self, name, occupation):
-#         print("constructor Always called")
-#         self.name = name
-#         self.occupation = occupation
 
-#     def information(self):
-#         print(f"{self.name} is a fresher\nJoined as {self.occupation}")
+# def price_alltoment(fx):
+#     def price_loading():
+#         print("price loading........\n")
+#         fx()
+#         print("Price Displayed")
 
-
-# b = person("Raja", "SDE")  # this store the value in name and occupation
-# # whenver the info is called then it prints the program
-# b.information()  # this prints the value given in string of function
-
-
-# # Base Code:
-# class book:
-#     book_name = "Enter Your Book Name:\n"
-#     book_author = "Enter Your Book author:\n"
-#     book_price = "Enter Your Book price:\n"
-
-#     def details_book(self):
-#         print(
-#             f'The "{self.book_name}" is written by {self.book_author},\nthe best book under {self.book_price}'
-#         )
-
-
-# a = book()
-# a.book_name = "venice"
-# a.book_author = "shakespeare"
-# a.book_price = "400 /-"
-# a.details_book()
+#     return price_loading()
 
