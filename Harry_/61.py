4ad47aa003e13fbc36031816e2649c400915e8cb
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-09 05:11:06 +0530

    day56

---
+++ Harry_/61.py
@@ -0,0 +1,32 @@
+# inheritance: class====> modified class
+# this simply converts class into a modified class
+
+
+class student:
+    def __init__(self, name, roll_number):
+        self.name = name
+        self.roll_number = roll_number
+
+    def student_details(self):
+        print(
+            f"student named {self.name} whose roll number {self.roll_number} is a boy "
+        )
+
+
+obj = student("raj", 98)
+obj.student_details()
+
+# now new details branch has to be added
+
+
+class update1(student):
+    def __init__(self, branch):
+        self.branch = branch
+
+    def student_details(self):
+        print("Whose branch is Aiml")
+
+
+obj = update1("Aiml")
+obj.student_details()
+obj.student_details()
--- Harry_/tempCodeRunnerFile.py
+++ Harry_/tempCodeRunnerFile.py
@@ -1,10 +1,28 @@
+# inheritance: class====> modified class
+# this simply converts class into a modified class
 
 
-# def price_alltoment(fx):
-#     def price_loading():
-#         print("price loading........\n")
-#         fx()
-#         print("Price Displayed")
+class student:
+    def __init__(self, name, roll_number):
+        self.name = name
+        self.roll_number = roll_number
 
-#     return price_loading()
+    def student_details(self):
+        print(
+            f"student named {self.name} whose roll number {self.roll_number} is a boy "
+        )
 
+obj = student("raj", 98)
+obj.student_details()
+
+# now new details branch has to be added 
+
+class update1(student):
+    def __init__(self, branch):
+        self.branch=branch
+
+    def student_details(self):
+        print("Whose branch is Aiml")
+
+obj = update1("Aiml")
+obj.student_details()
