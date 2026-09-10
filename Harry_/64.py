521baf0bff63eae0d458987fa142a82a17e38597
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-11 03:37:42 +0530

    day56

--- Harry_/64.py
+++ Harry_/64.py
@@ -1,39 +1,47 @@
 # library management system:
 class library:
 
+    books = [
+                "Python Crash Course",
+                "Automate the Boring Stuff with Python",
+                "The Pragmatic Programmer",
+                "Clean Code",
+                "Fluent Python",
+                "Head First Python",
+                "Effective Python",
+                "Learning Python",
+                "Think Python",
+                "Python Cookbook",
+            ]
+
     def list_book(self):
         print("<<<<<The Books>>>>>")
-        self.books = [
-            "Python Crash Course",
-            "Automate the Boring Stuff with Python",
-            "The Pragmatic Programmer",
-            "Clean Code",
-            "Fluent Python",
-            "Head First Python",
-            "Effective Python",
-            "Learning Python",
-            "Think Python",
-            "Python Cookbook",
-        ]
         for i in range(0, len(self.books)):
             print(i, "===>", self.books[i])
         print("<<<<<These are the books>>>>>")
-    
-    books = []
+
+    cart_books = []
 
     def add_book(self):
-        user_add_book = input("<<<<<<<<<<<<<<<<<<<<<<Enter Your Choice:>>>")
-        self.book
-        self.book.append(self.book[user_add_book])
+        book_index_status = int(input("Enter Your book Index:\n"))
+        for i in books:
+            if book_index_status== i :
+                print(books[i])
+            else:
+                continue
+            pass
+        # 1. first store the index  
+        # 2. now go to the self.books 
+        # 3. read the index and return in the book name from that index
+
         pass
 
     def books_in_cart(self):
-        for i in range(0, len(self.books)):
-            if len(self.books) == 0:
+            if len(self.cart_books) == 0:
                 print("No Books in the cart\n")
             else:
-                print(self.books[i])
-
+                for i in range(0, len(self.cart_books)):
+                    print(self.cart_books[i])
 
 lib = library()
 
