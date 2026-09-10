3734b316fea9570daae78e79d8f43b649852816b
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-11 03:51:16 +0530

    day56

--- practice/01.py
+++ practice/01.py
@@ -1,14 +1,15 @@
-books =[
-            "Python Crash Course",
-                "Automate the Boring Stuff with Python",
-                "The Pragmatic Programmer",
-                "Clean Code",
-                "Fluent Python",
-                "Head First Python",
-                "Effective Python",
-                "Learning Python",
-                "Think Python",
-                "Python Cookbook"
-        ]
-for i in range(1,len(books)):
-    print
+# pizza
+
+
+class pizza:
+    def __init__(self, size, topings):
+        print("<<Welcome to Our Restaurant>>\t\n")
+        size_of_pizza = input(
+            "Size of Pizza You Would Like To Have:\n CLick 1====> for the small\nCLick 2====> for the medium\nCLick 3====> for the large"
+        )
+        topping_on_pizza = input("Among The Topping You Would Like To Have:\t\n")
+
+        def describe(self):
+            print(
+                f"Your Pizza is {size_of_pizza} and it has {topping_on_pizza} toppings on the pizza"
+            )
