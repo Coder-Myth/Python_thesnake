22261ae861d37d4768b53005df7f640356783d23
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-11 15:00:55 +0530

    day56

--- practice/01.py
+++ practice/01.py
@@ -2,14 +2,38 @@
 
 
 class pizza:
-    def __init__(self, size, topings):
+    def __init__(self, size, topping):
+        # mostly covers frontend logic
         print("<<Welcome to Our Restaurant>>\t\n")
-        size_of_pizza = input(
-            "Size of Pizza You Would Like To Have:\n CLick 1====> for the small\nCLick 2====> for the medium\nCLick 3====> for the large"
-        )
-        topping_on_pizza = input("Among The Topping You Would Like To Have:\t\n")
 
-        def describe(self):
-            print(
-                f"Your Pizza is {size_of_pizza} and it has {topping_on_pizza} toppings on the pizza"
+        self.size_of_pizza = input(
+            "Size of Pizza:\n1 ===> Small\n2 ===> Medium\n3 ===> Large"
+        )
+        self.size_of_pizza = int(self.size_of_pizza)
+        if self.size_of_pizza == 1:
+            return "Small"
+        elif self.size_of_pizza == 2:
+            return "Medium"
+        elif self.size_of_pizza == 3:
+            return "Large"
+
+        topping = []
+        while True:
+            self.topping_on_pizza = input(
+                "Among The Topping You Would Like To Have:\t\nClick 1 for Tomato\nClick 2 for Capsicum\nClick 3 for sauce\nClick 4 for Mayonise\nClick 5 to Exit and bill"
             )
+            self.topping_on_pizza = int(self.topping_on_pizza)
+            if self.topping_on_pizza == 1:
+                return "Tomato"
+            elif self.topping_on_pizza == 2:
+                return "Capsicum"
+            elif self.topping_on_pizza == 3:
+                return "Sauce"
+            elif self.topping_on_pizza == 4:
+                return "Mayonise"
+            self.topping_on_pizza.append(topping)
+
+    def describe(self):
+        print(
+            f"Your Pizza is {self.size_of_pizza} and it has {self.topping_on_pizza} toppings on the pizza"
+        )
