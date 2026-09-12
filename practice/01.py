d77cd2cd04e19138aa35946e644ae9da796d4d4f
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-13 05:13:49 +0530

    day56

--- practice/01.py
+++ practice/01.py
@@ -18,19 +18,19 @@ class pizza:
 
     def add_toppings(self):
         while True:
-            self.topping_on_pizza = input(
+            topping_on_pizza = input(
                 "Among The Topping You Would Like To Have:\t\nClick 1 for Tomato\nClick 2 for Capsicum\nClick 3 for sauce\nClick 4 for Mayonise\nClick 5 to Exit and bill"
             )
-            self.topping_on_pizza = int(self.topping_on_pizza)
-            if self.topping_on_pizza == 1:
+            topping_on_pizza = int(topping_on_pizza)
+            if topping_on_pizza == 1:
                 self.topping.append("Tomato")
-            elif self.topping_on_pizza == 2:
+            elif topping_on_pizza == 2:
                 self.topping.append("Capsicum")
-            elif self.topping_on_pizza == 3:
+            elif topping_on_pizza == 3:
                 self.topping.append("Sauce")
-            elif self.topping_on_pizza == 4:
+            elif topping_on_pizza == 4:
                 self.topping.append("Mayonise")
-            elif self.topping_on_pizza == 5:
+            elif topping_on_pizza == 5:
                 break
 
     def describe(self):
