393d9df48d0a7b578bb184439c84dd439bd33d1a
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-13 05:07:51 +0530

    day56

--- practice/01.py
+++ practice/01.py
@@ -4,43 +4,42 @@
 class pizza:
     def __init__(self, size, topping):
         print("<<Welcome to Our Restaurant>>\t\n")
-        self.size = input(
-            "Size of Pizza:\n1 ===> Small\n2 ===> Medium\n3 ===> Large"
-        )
+        self.size = input("Size of Pizza:\n1 ===> Small\n2 ===> Medium\n3 ===> Large")
         self.topping = []
 
-    def size_of_pizza(self):
-        self.size = int(self.size)
-        if self.size == 1:
-            self.size == "Small"
-        elif self.size == 2:
-            self.size =="Medium"
-        elif self.size == 3:
-            self.size =="Large"
+    def size_of_pizza(self, size_of_pizza):
+        self.size_of_pizza = int(self.size)
+        if self.size_of_pizza == 1:
+            self.size_of_pizza == "Small"
+        elif self.size_of_pizza == 2:
+            self.size_of_pizza == "Medium"
+        elif self.size_of_pizza == 3:
+            self.size_of_pizza == "Large"
 
+    def add_toppings(self):
         while True:
             self.topping_on_pizza = input(
                 "Among The Topping You Would Like To Have:\t\nClick 1 for Tomato\nClick 2 for Capsicum\nClick 3 for sauce\nClick 4 for Mayonise\nClick 5 to Exit and bill"
             )
             self.topping_on_pizza = int(self.topping_on_pizza)
             if self.topping_on_pizza == 1:
-                self.topping == "Tomato"
+                self.topping.append("Tomato")
             elif self.topping_on_pizza == 2:
-                return "Capsicum"
+                self.topping.append("Capsicum")
             elif self.topping_on_pizza == 3:
-                return "Sauce"
+                self.topping.append("Sauce")
             elif self.topping_on_pizza == 4:
-                return "Mayonise"
-            self.topping_on_pizza.append(topping)
+                self.topping.append("Mayonise")
+            elif self.topping_on_pizza == 5:
+                break
 
     def describe(self):
         print(
-            f"Your Pizza is {self.size_of_pizza} and it has {self.topping_on_pizza} toppings on the pizza"
+            f"Your Pizza is {self.size} and it has {self.add_toppings} toppings on the pizza"
         )
 
 
-
 p1 = pizza(None, None)
-p1.size_of_pizza() 
+p1.size_of_pizza()
 p1.add_toppings()
 p1.describe()
