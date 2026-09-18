604d46241d1dfa8b8006252acc352c3496d2f67f
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-18 21:23:07 +0530

    day

---
+++ Harry_/66.py
@@ -0,0 +1,6 @@
+#instance variable and class variable :
+
+#instance variable: this is a variable for the specific object 
+
+#class variable : this is a variable for the whole class whenever this is defined remains same till the instance variable is given to the object
+
