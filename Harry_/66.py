d60318aeea0bbeede5c6afa3e39ec368643d6e0d
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-19 12:48:13 +0530

    day56

--- Harry_/66.py
+++ Harry_/66.py
@@ -11,27 +11,36 @@ class student:
     def __init__(self, student_name, roll_number, fees_pending):
         self.student_name = student_name
         self.roll_number = roll_number
-        self.__fees_pending = fees_pending
+        self._fees_pending = fees_pending
 
     @property
-    def Fees(self):
-        return self.__fees_pending
+    def fees_pending(self):
+        return self._fees_pending
+
+    @fees_pending.setter
+    def fees_pending(self, new_amount):
+        self._fees_pending = new_amount
 
     def show_details(self):
         print(
-            f"Roll Number {self.roll_number} named {self.student_name} studied from this our school {self.school_name} "
+            f"Roll Number {self.roll_number} named {self.student_name} studied from this our school {self.school_name} {self._fees_pending} "
         )
 
 
-s1 = student("Raja", 45)
+s1 = student("Raja", 45, 45000)
 s1.show_details()
+print(s1.fees_pending)
 # for s1 this uses class variable
 
-s2 = student("Dhananjay", 38)
+s1.fees_pending = 25000
+# calls the setter — sets self._fees_pending to 25000
+s1.show_details()
+
+s2 = student("Dhananjay", 38, 0)
 s2.school_name = "AITR"  # this is an instance variable
 s2.show_details()
 # for s2 this uses instance varaible i.e. it only changes for the student s2
 
-s3 = student("Dhairya", 37)
+s3 = student("Dhairya", 37, 4500)
 s3.show_details()
 # this again uses class variable i.e. school_name
