f3845857fd85a07654c8be89a12884bc0bf29312
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-20 15:26:52 +0530

    day56

--- Harry_/67.py
+++ Harry_/67.py
@@ -1,9 +1,1 @@
-# clear the cutter: using os module rename the files in 01,02,03........999  not all files should be created but no. of files there in folder should be renamed in a sequential way 
-
-import os 
-
-file_type_input= int(input("Enter \n1==> pdf\n2==> png"))
-for i in range(len(folder)):
-    os.rename("","01.png")
-
-# left this excercise not enough prepared
+#solution to the library system
