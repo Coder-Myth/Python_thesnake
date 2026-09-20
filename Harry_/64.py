f27e0f9edcf547c9e6280c0c9eccf6ccfcec1dcb
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-20 14:19:11 +0530

    day56

--- Harry_/64.py
+++ Harry_/64.py
@@ -112,7 +112,7 @@ class library:
             print(i + 1, "==>", self.books_added[i])
 
 
-user_1 = library(1)
+# user_1 = library(1)
 user_1 = library(2)
-user_1 = library(3)#this checks the eariler list in init not the new one 
+# user_1 = library(3)#this checks the eariler list in init not the new one 
 user_1.books_in_cart()#this checks the new list i.e. the new one list 
