472af6c01f4e4c0e22d2f90e5415228019f7e52a
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-21 19:33:24 +0530

    day56

--- Harry_/69.py
+++ Harry_/69.py
@@ -1,1 +1,21 @@
-#class methods:
+# class methods: a method that allows you to change the class i.e. not instance variable the class variable
+# instance only changes the details for specific employee but class method allows you to change the class variable
+
+
+class employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def retire_age(self, retire_age):
+        self.retire_age = self.age - 60
+        return self.retire_age
+
+    def details(self):
+        print(
+            f"The Employee {self.name}\nhe is {self.age} years old\nIN {self.retire_age} years he will get retired."
+        )
+
+
+E1 = employee("Raja", 43)
+E1.details()
