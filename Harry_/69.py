074ac8745d217dc52278f10eef0166e38277d3b4
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-21 19:58:09 +0530

    day56

--- Harry_/69.py
+++ Harry_/69.py
@@ -1,8 +1,9 @@
 # class methods: a method that allows you to change the class i.e. not instance variable the class variable
 # instance only changes the details for specific employee but class method allows you to change the class variable
 
-#important notice 
+# important notice
 # you can use the  function in class showdetails on in a f-strings
+
 
 class employee:
     retirement_age = 60
@@ -10,10 +11,14 @@ class employee:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+
     def retire_age(self):
-        left_years= 60 - self.age
+        left_years = self.retirement_age - self.age
         return left_years
+
+    # @classmethod
+    def change_retirement_age(self, new_age):
+        self.retirement_age = new_age
 
     def details(self):
         print(
@@ -23,3 +28,9 @@ class employee:
 
 E1 = employee("Raja", 43)
 E1.details()
+# this function only changes the retirement year in E2
+E2 = employee("Rakesh", 55)
+E2.change_retirement_age(57)
+E2.details()
+
+# But If I Use @classmethod over it that is commented then the class variable will be changed
