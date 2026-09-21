8ca1df07c4276768e65b941930619537cdc7332b
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-21 19:47:46 +0530

    day56

--- Harry_/69.py
+++ Harry_/69.py
@@ -1,6 +1,8 @@
 # class methods: a method that allows you to change the class i.e. not instance variable the class variable
 # instance only changes the details for specific employee but class method allows you to change the class variable
 
+#important notice 
+# you can use the  function in class showdetails on in a f-strings
 
 class employee:
     retirement_age = 60
@@ -8,13 +10,14 @@ class employee:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
-    def retire_age(self, retire_age):
-        return self.retire_age
+        
+    def retire_age(self):
+        left_years= 60 - self.age
+        return left_years
 
     def details(self):
         print(
-            f"The Employee {self.name}\nhe is {self.age} years old\nIN {self.retire_age} years he will get retired."
+            f"The Employee {self.name}\nhe is {self.age} years old\nIN {self.retire_age()} years he will get retired."
         )
 
 
