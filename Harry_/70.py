af13755c937daa4b044e5d42491a5836e084d296
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-25 19:18:23 +0530

    day56

--- Harry_/70.py
+++ Harry_/70.py
@@ -3,10 +3,21 @@ class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
-    
+
+    @classmethod
+    def fromStr(cls,string):
+        
+        pass
     def details(self):
         print(f"Name--->{self.name}\nAge--->{self.salary}")
 
-# what if the input is given in single string but seperated by a - for this we create a list that has 
+
+# what if the input is given in single string but  a - for this we create a list that has the words seperatedb= Employee() by -
+# using class methods as a constructors
 a = Employee("Raja", 18000)
 a.details()
+
+
+string = "Raj-120000"
+b = Employee.fromStr(string)
+b.details()
