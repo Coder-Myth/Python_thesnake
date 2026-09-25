ad100423329233b2642cca90e4bbde0634dabede
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-09-25 19:10:49 +0530

    day56

--- Harry_/70.py
+++ Harry_/70.py
@@ -1,1 +1,12 @@
-#class methods as constructor:
+# class methods as constructor:
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    def details(self):
+        print(f"Name--->{self.name}\nAge--->{self.salary}")
+
+# what if the input is given in single string but seperated by a - for this we create a list that has 
+a = Employee("Raja", 18000)
+a.details()
